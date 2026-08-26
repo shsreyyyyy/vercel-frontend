f69b3860030b4510454fe3ef9e9660d78304061b
Shreyas <shreyashagharkar47@gmail.com>
2026-08-26 10:02:51 +0530

    frontend changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect, } from 'react'
+import  { useContext,useEffect, } from 'react'
 import { AppContext } from '../context/cartContext'
 import { WishlistContext } from '../context/wishlistContext'
 import { OrderContext } from '../context/orderContext'
